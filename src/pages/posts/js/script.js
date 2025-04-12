1be5bce044a1fd373220e53bbb091f29e79b3dce
lingqiu1700 <162371509+lingqiu1700@users.noreply.github.com>
2025-04-13 00:48:18 +0800

    Update script.js

--- src/pages/posts/js/script.js
+++ src/pages/posts/js/script.js
@@ -1,19 +1,1 @@
-    // 使用 Howler.js 创建一个 Howl 对象
-    const sound = new Howl({
-        src: ['https://github.com/lingqiu1700/isui/raw/refs/heads/master/src/pages/sounds/t3.ogg'], // 音频文件路径
-        html5: true // 确保使用 HTML5 Audio
-    });
 
-    // 获取播放和暂停按钮
-    const playButton = document.getElementById('play');
-    const pauseButton = document.getElementById('pause');
-
-    // 为播放按钮添加点击事件
-    playButton.addEventListener('click', () => {
-        sound.play();
-    });
-
-    // 为暂停按钮添加点击事件
-    pauseButton.addEventListener('click', () => {
-        sound.pause();
-    });
